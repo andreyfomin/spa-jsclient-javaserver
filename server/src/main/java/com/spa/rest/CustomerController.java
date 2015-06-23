d5229bdd0413cc97d8ad5cc348318e594109e677
andreyfomin <AndreyFomin!(72gh>
2015-06-23 08:04:46 +0300

    add paging sorting support

--- server/src/main/java/com/spa/rest/CustomerController.java
+++ server/src/main/java/com/spa/rest/CustomerController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +34,6 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.OK)
     public SrvSideTableParamResMessage getServerSideProcessedCustomers(@RequestBody SrvSideTableParamReqMessage reqMessage) {
         SrvSideTableParamResMessage srvSideTableParamResMessage = new SrvSideTableParamResMessage();
-
-        Pageable page = new PageRequest(reqMessage.getStart() / reqMessage.getLength(), reqMessage.getLength());
-
 
         final Page allCustomers = customerService.findAllCustomers(
                 new PageRequest(
